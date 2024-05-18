d04eb7e6415067b63322f6a0c6032648d4b28ecc
Chamod Abeyrathne <chamodabeyrathna@gmail.com>
2024-05-18 22:11:39 +0100

    Formatted

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,12 @@
 const slider = document.querySelector('.slider');
 
 function activate(e) {
-  const items = document.querySelectorAll('.item');
-  e.target.matches('.next') && slider.append(items[0])
-  e.target.matches('.prev') && slider.prepend(items[items.length-1]);
+    const items = document.querySelectorAll('.item');
+    e.target.matches('.next') && slider.append(items[0])
+    e.target.matches('.prev') && slider.prepend(items[items.length - 1]);
 }
 
-document.addEventListener('click',activate,false);
+document.addEventListener('click', activate, false);
 
 // Search Null Validation
 
@@ -16,13 +16,13 @@ const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('searchButton');
 
 searchInput.addEventListener('keyup', () => {
-  const value = searchInput.value.trim(); // Trim whitespace
-  if (!value) {
-    searchInput.classList.add('is-invalid'); // Add Bootstrap error class
-    searchButton.disabled = true; // Disable button
-  } else {
-    searchInput.classList.remove('is-invalid');
-    searchButton.disabled = false;
-  }
+    const value = searchInput.value.trim(); // Trim whitespace
+    if (!value) {
+        searchInput.classList.add('is-invalid'); // Add Bootstrap error class
+        searchButton.disabled = true; // Disable button
+    } else {
+        searchInput.classList.remove('is-invalid');
+        searchButton.disabled = false;
+    }
 });
 
